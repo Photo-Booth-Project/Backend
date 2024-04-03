2aa9c46aa94dfcaf8aaa6e5ec4b83b6680434523
hyoh <dhgkdud20@12cm.co.kr>
2024-04-03 17:23:46 +0900

    init

---
+++ src/main/java/lumina/photobooth/PhotoBoothApplication.java
@@ -0,0 +1,13 @@
+package lumina.photobooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBoothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoBoothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lumina/photobooth/PhotoBoothApplicationTests.java
@@ -0,0 +1,13 @@
+package lumina.photobooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoBoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
